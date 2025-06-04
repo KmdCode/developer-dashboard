b8c70d68da12c94b4e6aba5c3c5d7220b2816340
karabomodise260@gmail.com <karabomodise260@gmail.com>
2025-06-04 11:34:10 +0200

    userCard update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react"
+import Homepage from "./pages/Homepage"
 
 export const App = () => {
 
   return (
     <>
       <h1>Developer Dashboard</h1>
+      <Homepage/>
     </>
   )
 }
---
+++ src/components/userCard/UserCard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const UserCard = () => {
+
+    return(
+        <>
+            <div className="container">
+                {/* <img>Image</img> */}
+                <h2>Username: Karabo</h2>
+                <button>Profile</button>
+                <button>Add to favorite</button>
+            </div>
+        
+        </>
+    )
+}
+export default UserCard;
